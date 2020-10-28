dadcd9e1f240191b11c7bdf72e1cac5fb70ff9ca
andrea-dintino <andrea@tabbles.net>
2020-10-28 19:09:05 +0100

    disclaimer

--- src/components/disclaimer/Disclaimer.js
+++ src/components/disclaimer/Disclaimer.js
@@ -20,7 +20,11 @@ const Disclaimer = props => {
             <Modal.Body>
                 <h3 className='my-3' style={{ textAlign: 'center' }}>Disclaimer</h3>
                 <div className='pb-3 text-justify px-2'>
-                    Det er en kendsgerning, at man bliver distraheret af læsbart indhold på en side, når man betragter dens layout. Meningen med at bruge Lorem Ipsum er, at teksten indeholder mere eller mindre almindelig tekstopbygning i modsætning til "Tekst her - og mere tekst her", mens det samtidigt ligner almindelig tekst. Mange layoutprogrammer og webdesignere bruger Lorem Ipsum som fyldtekst. En søgning på Lorem Ipsum afslører mange websider, som stadig er på udviklingsstadiet. Der har været et utal af variationer, som er opstået enten på grund af fejl og andre gange med vilje (som blandt andet et resultat af humor).
+                This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
+                </div>
+                <div className='pb-3 text-justify px-2'></div>
+                <div className='pb-3 text-justify px-2'>
+                All the Zencode scripts stored in this service are do belong to the respetive authors and all released under Apache2 license. Find a list of  GNU Affero General Public License at: https://www.gnu.org/licenses/
                 </div>
             </Modal.Body>
             <Modal.Footer className="d-flex justify-content-start p-5">
