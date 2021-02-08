ecc8799a28df51659bcf6af840df6d9e1abc18cc
seabassdk <s.blichfeldt@gmail.com>
2021-02-08 16:11:26 +0100

    changed apis to production

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,51 +1,51 @@
 // FOR DEVELOPMENT
-export const register = 'http://localhost:3010/user/register';
-export const login = 'http://localhost:3010/user/login';
+// export const register = 'http://localhost:3010/user/register';
+// export const login = 'http://localhost:3010/user/login';
 
-export const saveContractUri = 'http://localhost:3010/data/save/contract';
-export const saveTypeUri = 'http://localhost:3010/data/save/'; //:type
+// export const saveContractUri = 'http://localhost:3010/data/save/contract';
+// export const saveTypeUri = 'http://localhost:3010/data/save/'; //:type
 
-export const loadContractsUri = 'http://localhost:3010/data/load/contracts';
-export const loadTypeUri = 'http://localhost:3010/data/load/'; // :type
+// export const loadContractsUri = 'http://localhost:3010/data/load/contracts';
+// export const loadTypeUri = 'http://localhost:3010/data/load/'; // :type
 
-export const updateContractUri = 'http://localhost:3010/data/update/contract/'; // :index
-export const updateContractByindexUri = 'http://localhost:3010/data/update/contractfield/'; // :index
-export const updateTypeUri = 'http://localhost:3010/data/update/'; //:type/:index/:field
+// export const updateContractUri = 'http://localhost:3010/data/update/contract/'; // :index
+// export const updateContractByindexUri = 'http://localhost:3010/data/update/contractfield/'; // :index
+// export const updateTypeUri = 'http://localhost:3010/data/update/'; //:type/:index/:field
 
-export const deleteContractByIndexUri = 'http://localhost:3010/data/delete/contract/'; //:index
+// export const deleteContractByIndexUri = 'http://localhost:3010/data/delete/contract/'; //:index
 
-export const switchContractByIndexUri = 'http://localhost:3010/data/contract/switch/'; //:index
+// export const switchContractByIndexUri = 'http://localhost:3010/data/contract/switch/'; //:index
 
-export const linkToSwaggerUri = 'http://localhost:3010/docs/'; //:username
+// export const linkToSwaggerUri = 'http://localhost:3010/docs/'; //:username
 
-export const getDockerFileUri = 'http://localhost:3010/docker/';
+// export const getDockerFileUri = 'http://localhost:3010/docker/';
 
-export const getRRScriptUri = 'http://localhost:3010/rrscript/';
+// export const getRRScriptUri = 'http://localhost:3010/rrscript/';
 
-export const getContractsUri = 'http://localhost:3010/exportcontracts/';
+// export const getContractsUri = 'http://localhost:3010/exportcontracts/';
 
 // FOR PRODUCTION
-// export const register = '/user/register';
-// export const login = '/user/login';
+export const register = '/user/register';
+export const login = '/user/login';
 
-// export const saveContractUri = '/data/save/contract';
-// export const saveTypeUri = '/data/save/'; //:type
+export const saveContractUri = '/data/save/contract';
+export const saveTypeUri = '/data/save/'; //:type
 
-// export const loadContractsUri = '/data/load/contracts';
-// export const loadTypeUri = '/data/load/'; // :type
+export const loadContractsUri = '/data/load/contracts';
+export const loadTypeUri = '/data/load/'; // :type
 
-// export const updateContractUri ='/data/update/contract/'; // :index
-// export const updateContractByindexUri ='/data/update/contractfield/'; // :index
-// export const updateTypeUri = '/data/update/'; //:type/:index/:field
+export const updateContractUri ='/data/update/contract/'; // :index
+export const updateContractByindexUri ='/data/update/contractfield/'; // :index
+export const updateTypeUri = '/data/update/'; //:type/:index/:field
 
-// export const deleteContractByIndexUri =  '/data/delete/contract/'; //:index
+export const deleteContractByIndexUri =  '/data/delete/contract/'; //:index
 
-// export const switchContractByIndexUri =  '/data/contract/switch/'; //:index
+export const switchContractByIndexUri =  '/data/contract/switch/'; //:index
 
-// export const linkToSwaggerUri = '/docs/'; //:username
+export const linkToSwaggerUri = '/docs/'; //:username
 
-// export const getDockerFileUri = '/docker/';
+export const getDockerFileUri = '/docker/';
 
-// export const getRRScriptUri = '/rrscript/';
+export const getRRScriptUri = '/rrscript/';
 
-// export const getContractsUri = '/exportcontracts/';
+export const getContractsUri = '/exportcontracts/';
